ca144c670ea865ed6e423510943ab5fa11aba0d0
olivejoys <olivejoy@sheridancollege.ca>
2024-08-22 22:54:49 -0400

    First commit.

---
+++ src/main/java/com/example/guest_flow/GuestFlowApplication.java
@@ -0,0 +1,13 @@
+package com.example.guest_flow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuestFlowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuestFlowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/guest_flow/GuestFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.guest_flow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuestFlowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
