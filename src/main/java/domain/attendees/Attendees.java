8546da682c404dd31ffe71b559e9ec40d335b993
olivejoys <olivejoy@sheridancollege.ca>
2024-08-24 19:10:59 -0400

    feat: created Attendees Entity

---
+++ src/main/java/domain/attendees/Attendees.java
@@ -0,0 +1,38 @@
+package domain.attendees;
+
+import domain.event.Event;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class Attendees {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String email;
+
+    @ManyToOne
+    @JoinColumn(name = "event_id", nullable = false)
+    private Event event;
+
+
+    @Column(nullable = false, name = "day_created")
+    private LocalDateTime day_created;
+
+}
--- src/main/java/domain/event/Event.java
+++ src/main/java/domain/event/Event.java
@@ -1,11 +1,17 @@
-package com.example.guest_flow.Entities;
+package domain.event;
 
 import jakarta.persistence.*;
-import lombok.Getter;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
 
 @Entity
 @Table(name="events")
 @Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Event {
     @Id
     @Column(nullable=false) // this column cannot be null
@@ -22,5 +28,11 @@ public class Event {
     @Column(nullable=false, name="max_attendees")
     private Integer max_attendees;
 
+   @ManyToOne
+   @JoinColumn(name = "event_id", nullable = false)
+    private Event event;
+
+   @Column(name = "created_at", nullable = false)
+   private LocalDateTime createdAt;
 
 }
