d4e0786c063bc76add41c3e6518bdbff8655efb8
olivejoys <olivejoy@sheridancollege.ca>
2024-08-26 15:40:55 -0400

    feat: created Event Repository

--- src/main/java/com/example/guest_flow/GuestFlowApplication.java
+++ src/main/java/com/example/guest_flow/GuestFlowApplication.java
@@ -2,12 +2,15 @@ package com.example.guest_flow;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
+@EntityScan(basePackages = "domain.event") //added an notation to fix the eventsRepoository
 @SpringBootApplication
 public class GuestFlowApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GuestFlowApplication.class, args);
+
 	}
 
 }
---
+++ src/main/java/com/example/guest_flow/repositories/EventRepository.java
@@ -0,0 +1,7 @@
+package com.example.guest_flow.repositories;
+
+import domain.event.Event;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EventRepository extends JpaRepository<Event,String> {
+}
